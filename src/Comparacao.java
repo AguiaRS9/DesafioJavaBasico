01f8654aae2ab14560702d8f2d0883fd187b1a15
Gustavo Cunha <guga.cunha123@hotmail.com>
2025-11-05 23:02:40 -0300

    Adição dos desafios finais

--- src/Comparacao.java
+++ src/Comparacao.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Comparacao {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro numero: ");
+        int primeiro = scanner.nextInt();
+
+        System.out.print("Digite o segundo numero: ");
+        int segundo = scanner.nextInt();
+
+        if (primeiro == segundo) {
+            System.out.println("Os numeros são iguais!");
+        } else if (primeiro > segundo) {
+            System.out.println("O primeiro número é maior que o segundo!");
+        } else {
+            System.out.println("O segundo numero é maior que o primeiro");
+        }
+    }
 }
